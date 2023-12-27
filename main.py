33c6e422e96680695e6f2b9e4a8bc27845d27b44
Pedro Pinango <ppinango@talpor.com>
2023-12-27 15:18:08 -0300

    add test main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    user_age = 25
+    user_name = "John"
+    page = 2
+
+    print(f"User age: {user_age}")
+
+
+class UserAge:
+    def __init__(self, age):
+        self.age = age
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def __str__(self):
+        return f"User age: {self.age}"
