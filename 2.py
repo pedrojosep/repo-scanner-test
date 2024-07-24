29160c2df5de2502cea2ab3c829cf559459c49fc
Pedro Pinango <ppinango@talpor.com>
2024-07-24 11:22:41 -0400

    add border cases

--- 2.py
+++ 2.py
@@ -1,1 +1,11 @@
+"""Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 10 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (4/4), 377 bytes | 377.00 KiB/s, done.
+Total 4 (delta 0), reused 0 (delta 0), pack-reused 0
+To github.com:pedrojosep/repo-scanner-test.git
+   78234c5..ae26df8  context-error -> context-error
+"""
+
 print("user_age is 25")
